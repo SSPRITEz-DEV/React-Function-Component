966e45cc8c65af23883946927bf3f7c7f3de655f
Tanawat Jituthai <sspritez@SSPRITEz-Mac.local>
2020-09-10 15:54:16 +0700

    add mini input

--- src/components/CatList/CatList.jsx
+++ src/components/CatList/CatList.jsx
@@ -8,21 +8,25 @@ export default function CatList() {
     const [catName, setCatName] = React.useState('sssssssss')
     */
 
+    const input_catname = React.createRef();
+
     const [catData, setCatData] = React.useState({
         name: 'ssatang',
         age: 22,
     })
-
     
-
     const btn_addname = () => {
         //setCatName('ssprite')
-
+        
+        //console.log(input_catname.current.value)
+        if(input_catname.current.value===''){
+            console.log('input again')
+        }
         // setCatData({name:'Por'}) age 
-        setCatData(prevState => {
+        else setCatData(prevState => {
             return {
                 ...prevState,
-                name: 'Por'
+                name: input_catname.current.value
             }
         })
     }
@@ -33,6 +37,7 @@ export default function CatList() {
             <div className={style.box}>
                 <p className={style.cat_name}>{catData.name}, {catData.age}</p>
             </div>
+            <input ref={input_catname} />
             <button onClick={btn_addname}>Set Name</button>
         </div>
     )
